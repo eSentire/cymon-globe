ecf174d23400c9e4240dc82123f7accf0ee3a165
Dan Petker <dan.petker@esentire.com>
2015-03-03 14:26:48 -0500

    Toned down the halo effect a bit

--- js/globe.js
+++ js/globe.js
@@ -95,7 +95,7 @@ DAT.Globe = function(container) {
         'varying vec3 vNormal;',
         'void main() {',
           'float intensity = pow( 0.8 - dot( vNormal, vec3( 0, 0, 1.0 ) ), 12.0 );',
-          'gl_FragColor = vec4( 1.0, 1.0, 1.0, 1.0 ) * intensity;',
+          'gl_FragColor = vec4( 0.5, 0.5, 0.5, 0.25 ) * intensity;',
         '}'
       ].join('\n')
     }
