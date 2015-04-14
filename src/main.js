d6657927aef45ac486b3450a3e529a695115c967
Dan Petker <dan.petker@esentire.com>
2015-04-14 15:25:28 -0400

    Converted main

---
+++ src/main.js
@@ -0,0 +1,34 @@
+import * as DAT from './globe';
+
+let globe = null;
+
+$( () => {
+  if( !Detector.webgl ) {
+    Detector.addGetWebGLMessage();
+  } else {
+    // Fetch data from the server
+    $.ajax({
+      url: 'http://cymon.io/api/publicajax/nexus/globe/?categories=1',
+      dataType: 'json',
+      error: ( jqXHR, textStatus, errorThrown ) => {
+        alert( `Could not load data. Reason: ${textStatus}` );
+      },
+      success: ( response ) => {
+        let container = document.getElementById( 'globe' );
+
+        THREE.ImageUtils.loadTexture( '../img/map2.jpg', undefined, ( texture ) => {
+          globe = DAT.createGlobe( container, texture );
+
+          globe.addData( response.data );
+          globe.initLegend();
+          doAnimate();
+        });
+      }
+    });
+  }
+});
+
+function doAnimate() {
+  requestAnimationFrame( doAnimate );
+  globe.render();
+}
